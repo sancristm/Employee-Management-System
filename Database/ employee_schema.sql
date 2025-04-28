1243f5755dc91241bf523c251bd2ed1ee1962d4c
sancristm <sancristma@gmail.com>
2025-04-28 07:03:07 +0300

    added sql script

---
+++ Database/ employee_schema.sql
@@ -0,0 +1,11 @@
+  CREATE DATABASE IF NOT EXISTS employeedb;
+USE employeedb;
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    department VARCHAR(255),
+    salary DECIMAL(10,2)
+);
